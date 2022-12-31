20f09a15419ebb23ceaf744fe8ca9c564b7c8a1b
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2022-12-31 02:07:46 -0500

    initial commit

---
+++ keyboards/gray_studio/space65r3/keymaps/shigure/config.h
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 Shigure
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// TT set to two taps
+#define TAPPING_TOGGLE 2
+
+/*  Handle GRAVESC combo keys */
+#define GRAVE_ESC_ALT_OVERRIDE
+    //Always send Escape if Alt is pressed
+#define GRAVE_ESC_CTRL_OVERRIDE
+    //Always send Escape if Control is pressed
+
+// tap vs hold time difference
+#define TAPPING_TERM 200
+
+// one shot tap settings
+#define ONESHOT_TAP_TAGGLE 3
+#define ONESHOT_TIMEOUT 1500
+
+#define RGB_DISABLE_TIMEOUT 900000 // number of milliseconds to wait until rgb automatically turns off
+#define RGB_DISABLE_WHEN_USB_SUSPENDED 1
+
+#define HSV_L1 71, 92, 80
+#define HSV_L2 36, 100, 100
+#define HSV_L3 330, 100, 93
+#define HSV_CAP 207, 100, 73
