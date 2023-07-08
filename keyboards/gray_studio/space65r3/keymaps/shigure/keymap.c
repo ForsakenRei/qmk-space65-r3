6ec6534c608c662fc1c09a824f0791c7a1035f69
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2023-07-08 15:46:27 -0400

    fix: keymap layer alignment

--- keyboards/gray_studio/space65r3/keymaps/shigure/keymap.c
+++ keyboards/gray_studio/space65r3/keymaps/shigure/keymap.c
@@ -49,28 +49,28 @@ bool _isWinKeyDisabled = false;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        CAP_L1,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
-        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   RSFT_L3,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             FN_L2,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+        CAP_L1,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_PGDN,
+        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   RSFT_L3,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             FN_L2,   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
     [1] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,  KC_INS,
-        _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, KC_CALC, _______, _______, SERACH,  TSK_MGR, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                            KC_DEL,                             _______, CTL_APP, _______, _______, _______),
+        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
+        _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, KC_CALC, _______, _______, SERACH,  TSK_MGR, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______,                            KC_DEL,                             _______, CTL_APP,          _______, _______, _______),
     [2] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, SFT_DEL, SFT_DEL, KC_SLEP,
-        _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_PAST, KC_PSLS, _______, _______,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_PENT, _______,
-        _______, _______, _______, _______, KC_CALC, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, _______, _______, _______, KC_MUTE,
-        _______, GUI_TOG, _______,                            _______,                            _______, _______, _______, _______, _______),
+        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_PAST, KC_PSLS, _______, _______,
+        _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_PENT,          _______,
+        _______, _______, _______, _______, KC_CALC, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, _______, _______,          _______, KC_MUTE,
+        _______, GUI_TOG, _______,                            _______,                            _______, _______,          _______, _______, _______),
     [3] = LAYOUT(
         _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE, _______, _______, KC_F13,  KC_F14,  _______, _______, RGB_SAI,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, RGB_SAD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EE_CLR,  RGB_HUI,
-        _______, _______, _______, _______, _______, _______, _______, _______, DC_MUTE, _______, _______, _______, _______, _______, RGB_HUD,
-        _______, _______, _______,                            _______,                            TO(0),   _______, RGB_VAD, _______, RGB_VAI),
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, RGB_SAD,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EE_CLR,           RGB_HUI,
+        _______, _______, _______, _______, _______, _______, _______, _______, DC_MUTE, _______, _______, _______, _______,          _______, RGB_HUD,
+        _______, _______, _______,                            _______,                            TO(0),   _______,          RGB_VAD, _______, RGB_VAI),
 };
 
 // RGB layers
